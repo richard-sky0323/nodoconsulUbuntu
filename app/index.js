4b10eb0f84338b00c573e6aea05a8e2a12724abc
richard-sky0323 <125331162+richard-sky0323@users.noreply.github.com>
2023-04-26 20:33:21 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const SERVICE_NAME='mymicroservice';
 const SERVICE_ID='m'+process.argv[2];
 const SCHEME='http';
-const HOST='99.5.100.2';
+const HOST='99.5.100.4';
 const PORT=process.argv[2]*1;
 const PID = process.pid;
 
